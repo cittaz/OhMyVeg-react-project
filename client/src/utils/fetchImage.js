26fda6318afa20d033a80eef2412ac646a05360a
cittaz <tommasocittadino.dev@gmail.com>
2023-03-04 19:18:43 +0100

    README updated

--- client/src/utils/fetchImage.js
+++ client/src/utils/fetchImage.js
@@ -1,5 +1,5 @@
 export async function fetchImage(prompt) {
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://ohmyveg-server.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
